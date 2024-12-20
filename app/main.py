946bc3226f794f7a772f2d54405a58e06add3d21
aqib <ahammad.aqib09939@gmail.com>
2024-12-20 23:17:44 +0530

    added cors

--- app/main.py
+++ app/main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI, Query, Response
-import requests
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+import requests
 import os
 
 load_dotenv()
 
 TELE_FILE_URL = os.getenv("TELE_FILE_URL")
+FRONTEND_URL = os.getenv("FRONTEND_URL")
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin=[FRONTEND_URL],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
@@ -16,7 +27,6 @@ async def root():
 @app.get('/getImage')
 def getImage(file_path: str = Query(...)):
     try:
-        # en_file_path = parse.urlencode(file_path)
         url = f'{TELE_FILE_URL}{file_path}'
         response = requests.get(url)
         return Response(content=response.content, media_type='image/png')
