07e6a7d5b8a3b6c139d153c8d36a03494bcae6af
aqib <ahammad.aqib09939@gmail.com>
2025-10-19 13:08:48 +0400

    add_middlewaree error

--- app/main.py
+++ app/main.py
@@ -23,12 +23,13 @@ router = APIRouter()
 
 app.add_middleware(
     CORSMiddleware,
-    UserValidationMiddleware,
     allow_origins=["https://vsnbrd-addidras-projects.vercel.app","https://vsnbrd.vercel.app","https://yearly-civil-starling.ngrok-free.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.add_middleware(UserValidationMiddleware)
 
 @app.get("/")
 async def hello():
